6ea850684d72428d1ecd26569b1d2cf3ee0321aa
BliinD <nathseitz7@gmail.com>
2024-09-20 17:06:47 +0200

    Ajout d'un config.php
    + simple, permet de connecter à la bdd juste en faisant une inclusion : ressort pas de texte sauf s'il y  a une erreur
    
    ATTENTION : il faut l'include dans tous nos ficher php, même ceux créés auparavant

---
+++ config.php
@@ -0,0 +1,31 @@
+
+<?php
+error_reporting(0);
+// Vérification si une session est déjà démarrée
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+
+// Vérification si un message de succès est présent
+if (isset($_SESSION['success_message'])) {
+    $success_message = $_SESSION['success_message'];
+    // Supprimer le message après l'avoir affiché une fois
+    unset($_SESSION['success_message']);
+}
+
+error_reporting(0); 
+// echo '<p>Connection à la base de données </p>';
+// Connexion à la base de données
+mysqli_report(MYSQLI_REPORT_OFF);
+$mysqli = @new mysqli("localhost", "root", "", "projetsql");
+
+if ( $mysqli->connect_errno ) {
+    echo "Impossible de se connecter à MySQL: errNum=" . $mysqli->connect_errno .
+    " errDesc=". $mysqli -> connect_error;
+    exit();
+    }
+   $mysqli->close(); 
+    // echo '<p>Connexion réussie !</p>';  
+?>
+
+
