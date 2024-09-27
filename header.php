e0071c8c2590fd184b6857112fc2bb0a4fb2c025
Emilien <emilien.derobert@gmail.com>
2024-09-27 16:42:12 +0200

    header
    - nom centrer
    - retrait des pages inutiles

--- header.php
+++ header.php
@@ -5,7 +5,8 @@
     <link rel="icon" type="image/vnd.icon" href="icon.png">
     <link rel="stylesheet" href="style.css">
 </head>
-<header>
+<body>
+    <header>
     <div id="start">
     
         <div id="mainHeader" style="display: flex; align-items: right;">
@@ -14,23 +15,7 @@
         <div id="mySidenav" class="sidenav">
         <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
         <a href="#">Projets</a>
-        <a href="#">Utilisateurs</a>
-        <a href="#">Sprint</a>
-        <a href="#">Tâches</a>
-        <a href="Page_Scrum_master.php">Scum Master</a>
-        <?php
-            include "config.php"; 
-            if (isset($_SESSION['logged_in']) && $_SESSION['logged_in'] === true):
-                // Vérifier si le pseudo et l'id de l'utilisateur sont définis dans la session
-                $pseudo = isset($_SESSION['pseudo']) ? $_SESSION['pseudo'] : 'Invité';
-                $idUser = isset($_SESSION['idUser']) ? $_SESSION['idUser'] : 0;
-            ?>
-                <a href='deconnexion.php'>Déconnexion</a>   
-                <?php else: ?>
-                <a href='connexion.php'>Connexion</a>   
-            <?php endif; ?> 
-
-
+        <a href='deconnexion.php'>Déconnexion</a>
         <script>
         function openNav() {
             document.getElementById("mySidenav").style.width = "250px";
@@ -51,10 +36,9 @@
     </div>
     
     <div id="center">
-        <a href="index.php"> <h1 style=" margin-left: 50px;">JARI</h1> </a>
+        <a href="index.php"> <h1>JARI</h1> </a>
     </div>
+    <div id="end"></div>    
     </header>
-<body>
-    
 </body>
 </html>
